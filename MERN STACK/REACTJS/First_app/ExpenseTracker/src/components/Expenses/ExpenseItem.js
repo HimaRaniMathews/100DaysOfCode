4c763a807a9704d2ead95a315b01fc4a3e4c8ded
himaranimathews <himaranimathews@gmail.com>
2022-01-16 20:54:37 +0530

    Added Modify Button

--- MERN STACK/REACTJS/First_app/ExpenseTracker/src/components/Expenses/ExpenseItem.js
+++ MERN STACK/REACTJS/First_app/ExpenseTracker/src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,26 @@
-import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+import "./ExpenseItem.css";
+import React,{useState} from "react";
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
+  const [title,setTitle]=useState(props.title);
+  
+  const clickHandler = () => {
+    setTitle("Update");
+    console.log(title);
+  };
+
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-    </div>
+      <button onClick={clickHandler}>Change Title</button>
+    </Card>
   );
-}
+};
 
 export default ExpenseItem;

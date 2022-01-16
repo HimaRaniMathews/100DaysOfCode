f244e58687dd89e114f36daac64d44d0cbf18c16
himaranimathews <himaranimathews@gmail.com>
2022-01-16 23:48:22 +0530

    Added Files

--- MERN STACK/REACTJS/First_app/ExpenseTracker/src/components/NewExpense/ExpenseForm.js
+++ MERN STACK/REACTJS/First_app/ExpenseTracker/src/components/NewExpense/ExpenseForm.js
@@ -18,12 +18,27 @@ const ExpenseForm = () => {
   const amountChangeHandler = (event) => {
     setEnteredAmount(event.target.value);
   };
+  const submitHandler = (event) => {
+    event.preventDefault();
+    
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate)
+    };
+
+    console.log(expenseData);
+    setEnteredTitle('');
+    setEnteredDate('');
+    setEnteredAmount('');
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input type="text" value={enteredTitle} onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
@@ -31,6 +46,7 @@ const ExpenseForm = () => {
             type="date"
             min="2019-01-01"
             max="2022-12-31"
+            value={enteredDate}
             onChange={dateChangeHandler}
           />
         </div>
@@ -40,6 +56,7 @@ const ExpenseForm = () => {
             type="number"
             min="0.01"
             step="0.01"
+            value={enteredAmount}
             onChange={amountChangeHandler}
           />
         </div>

805fbfaeb609986281f09c458414353f8754ee3a
himaranimathews <himaranimathews@gmail.com>
2022-01-17 23:07:10 +0530

    Added Files

--- MERN STACK/REACTJS/First_app/ExpenseTracker/src/components/NewExpense/ExpenseForm.js
+++ MERN STACK/REACTJS/First_app/ExpenseTracker/src/components/NewExpense/ExpenseForm.js
@@ -2,23 +2,21 @@ import React, { useState } from "react";
 
 import "./ExpenseForm.css";
 
-const ExpenseForm = () => {
-  const [enteredTitle, setEnteredTitle] = useState(''); 
-  const [enteredAmount, setEnteredAmount] = useState('');
-  const [enteredDate, setEnteredDate] = useState('');
-  
+const ExpenseForm = (props) => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+
   const titleChangeHandler = (event) => {
-    setEnteredTitle(event.target.value); // In here we are basically setting Title and updatating 
-  };                                        // automatially to EnteredTitle
+    setEnteredTitle(event.target.value); // In here we are basically setting Title and updatating
+  }; // automatially to EnteredTitle
 
   const amountChangeHandler = (event) => {
     setEnteredAmount(event.target.value); //
-  
   };
 
   const dateChangeHandler = (event) => {
     setEnteredDate(event.target.value);
-   
   };
 
   const submitHandler = (event) => {
@@ -30,46 +28,46 @@ const ExpenseForm = () => {
       date: new Date(enteredDate), // An object of all the updated or entered details
     };
 
-    console.log(expenseData);
-    setEnteredTitle('');
-    setEnteredAmount('');  // Clearing the fields after adding 
-    setEnteredDate('');
+    props.onSaveExpenseData(expenseData);
+    setEnteredTitle("");
+    setEnteredAmount(""); // Clearing the fields after adding
+    setEnteredDate("");
   };
 
   return (
     <form onSubmit={submitHandler}>
-      <div className='new-expense__controls'>
-        <div className='new-expense__control'>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
           <label>Title</label>
           <input
-            type='text'
+            type="text"
             value={enteredTitle}
             onChange={titleChangeHandler}
           />
         </div>
-        <div className='new-expense__control'>
+        <div className="new-expense__control">
           <label>Amount</label>
           <input
-            type='number'
-            min='0.01'
-            step='0.01'
+            type="number"
+            min="0.01"
+            step="0.01"
             value={enteredAmount}
             onChange={amountChangeHandler}
           />
         </div>
-        <div className='new-expense__control'>
+        <div className="new-expense__control">
           <label>Date</label>
           <input
-            type='date'
-            min='2019-01-01'
-            max='2022-12-31'
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
             value={enteredDate}
             onChange={dateChangeHandler}
           />
         </div>
       </div>
-      <div className='new-expense__actions'>
-        <button type='submit'>Add Expense</button>
+      <div className="new-expense__actions">
+        <button type="submit">Add Expense</button>
       </div>
     </form>
   );
--- MERN STACK/REACTJS/First_app/ExpenseTracker/src/components/NewExpense/NewExpense.js
+++ MERN STACK/REACTJS/First_app/ExpenseTracker/src/components/NewExpense/NewExpense.js
@@ -4,9 +4,17 @@ import './NewExpense.css';
 import ExpenseForm from './ExpenseForm';
 
 const NewExpense =()=>{
+
+    const saveExpenseData=(enteredExpenseData)=>{
+        const expenseData={
+            ...enteredExpenseData,
+            id: Math.random().toString()
+        };
+        console.log(expenseData);
+    }
     return(
         <div className="new-expense">
-        <ExpenseForm />
+        <ExpenseForm onSaveExpenseData={saveExpenseData} />
         </div>)
 }
 export default NewExpense;
